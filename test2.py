f0ace80f969e73472dfee78cc649bb965bc1d440
StratosPap <63465923+StratosPap@users.noreply.github.com>
2026-03-29 16:43:52 +0200

    Add files via upload

--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
 import os
 from pdf2image import convert_from_path
 from PIL import Image
+from pdf2image import pdfinfo_from_path
 import argparse
 
 # Terminal arguments
@@ -28,19 +29,30 @@ for file in sorted(os.listdir(input_folder)):
 
         print(f"Processing {file}...")
 
-        pages = convert_from_path(pdf_path, dpi=300)
+        info = pdfinfo_from_path(pdf_path)
+        total_pages = info["Pages"]
 
-        for i, page in enumerate(pages, start=1):
-            print(f"[{pdf_name}] Page {i}/{len(pages)}")
+        for i in range(1, total_pages + 1):
+
+            print(f"[{pdf_name}] Page {i}/{total_pages}")
+
+            page = convert_from_path(
+                pdf_path,
+                dpi=300,
+                first_page=i,
+                last_page=i,
+                thread_count=1
+            )[0]
+
             img = page.convert("L")
             img = img.resize((1700, 2400), Image.LANCZOS)
 
             filename = f"page-{i}.jpg"
             img.save(
-                os.path.join(output_folder, filename),
-                "JPEG",
-                quality=95,
-                dpi=(300, 300)
+            os.path.join(output_folder, filename),
+            "JPEG",
+            quality=95,
+            dpi=(300, 300)
             )
 
 print("All PDFs converted.")
